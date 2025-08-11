e73b6e81b2cb5716b1b659f90c749ff7c4925474
Seyhan Kadir ORHAN <seyhanceng2003@gmail.com>
2025-08-11 15:35:15 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 0.0.0.0'a bağlan ve LAN'dan erişimi aç
+    host: 'localhost',
     port: 5173,
     strictPort: true
   },
   preview: {
-    host: true,
+    host: 'localhost',
     port: 5173,
     strictPort: true
   }
